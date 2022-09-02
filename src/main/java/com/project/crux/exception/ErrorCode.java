b6d7a0d907866c38c12c0df34242c530b505ca1b
01192mg <01192mg@gmail.com>
2022-09-02 12:01:01 +0900

    feat: 예외처리 코드 작성 (#6)
    로그인 회원가입 request Dto
    response Dto 생성
    validation 의존성 추가
    ErrorCode 작성
    Exception Handler 작성

--- src/main/java/com/project/crux/exception/ErrorCode.java
+++ src/main/java/com/project/crux/exception/ErrorCode.java
@@ -1,4 +1,39 @@
 package com.project.crux.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public enum ErrorCode {
+
+    /*
+    400 Bad Request
+     */
+    DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "중복된 아이디가 존재합니다"),
+
+    DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "중복된 닉네임이 존재합니다"),
+
+    /*
+    401 UNAUTHORIZED : 인증되지 않은 사용자
+    */
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 틀렸습니다"),
+
+
+    /*
+    404 Not Found
+     */
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저 정보를 찾을 수 없습니다");
+
+
+
+
+
+
+    private final HttpStatus httpStatus;
+    private final String errorMessage;
+
+    ErrorCode(HttpStatus httpStatus, String errorMessage) {
+        this.httpStatus = httpStatus;
+        this.errorMessage = errorMessage;
+    }
 }
--- src/main/java/com/project/crux/exception/RestApiExceptionHandler.java
+++ src/main/java/com/project/crux/exception/RestApiExceptionHandler.java
@@ -1,4 +1,22 @@
 package com.project.crux.exception;
 
+import com.project.crux.domain.response.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class RestApiExceptionHandler {
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<ResponseDto<?>> handleApiRequestException(MethodArgumentNotValidException ex) {
+        return new ResponseEntity<>(ResponseDto.fail("BAD_REQUEST", ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {CustomException.class})
+    public ResponseEntity<ResponseDto<?>> handleApiRequestException(CustomException ex) {
+        return new ResponseEntity<>(ResponseDto.fail(ex.getErrorCode().getHttpStatus().name(), ex.getErrorCode().getErrorMessage()), ex.getErrorCode().getHttpStatus());
+    }
 }
