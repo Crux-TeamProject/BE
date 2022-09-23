b8c9b8572b34b7bd6ba7aa509df9e739a519c8f0
ctr0812 <andy0812@naver.com>
2022-09-23 14:52:06 +0900

    fix: 이니셜 데이터, 짐 스케줄러 수정 (#173)

--- src/main/java/com/project/crux/infrastructure/kakao/InitialDataRunner.java
+++ src/main/java/com/project/crux/infrastructure/kakao/InitialDataRunner.java
@@ -1,5 +1,7 @@
 package com.project.crux.infrastructure.kakao;
 
+import com.project.crux.gym.repository.GymRepository;
+import com.project.crux.gym.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -7,13 +9,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 
-//@Component
+@Component
 @RequiredArgsConstructor
 public class InitialDataRunner implements ApplicationRunner {
 
 
     private final KakaoApiService kakaoApiService;
-
+    private final GymRepository gymRepository;
+    private final ReviewRepository reviewRepository;
 
     @Override
     @Transactional
@@ -24,8 +27,14 @@ public class InitialDataRunner implements ApplicationRunner {
         double start_y = 33;
         double end_y = 39;
 
-        kakaoApiService.updateGymByKakaoApi(start_x,start_y,end_x,end_y);
-        kakaoApiService.updateGymImgByKakaoApi();
+        kakaoApiService.updateGymByKakaoApi(start_x, start_y, end_x, end_y);
+//        kakaoApiService.updateGymImgByKakaoApi();
+        gymRepository.findAll().stream()
+                .filter(gym -> !reviewRepository.findByGymOrderByIdDesc(gym).isEmpty())
+                .forEach(gym -> {
+                    gym.updateImg(reviewRepository.findByGymOrderByIdDesc(gym)
+                            .get(0).getReviewPhotoList().get(0).getImgUrl());
+                });
 
     }
 
